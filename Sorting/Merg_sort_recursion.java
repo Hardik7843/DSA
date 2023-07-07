fe491b5e391b91ec5ca8c97d76dfaeea796834a0
Hardik7843 <hardikprajapati7843@gmail.com>
2023-07-07 15:43:22 +0530

    Merge sort explained

--- Sorting/Merg_sort_recursion.java
+++ Sorting/Merg_sort_recursion.java
@@ -1,3 +1,24 @@
+/*
+ * mersort is used sort the array using recursion.
+ * It recursively breakes the array into two parts until
+ * only one element is remaining. then it merge two seperated array
+ * using two pointers. the two pointer points to start of both arrays.
+ * it compares both pointer's elements and smaller element get place in 
+ * new array i.e merged array.
+ * 
+ * Steps for mergesort 
+ * Step 1: Take the input array for seperating it into two parts.
+ *         if it only one element then direct return it. else calculate
+ *         mid position from where we need to divide the array.
+ * Step 2: Now give array (from start to mid) [mid exclusive] to 1st call of "mergesort" function (Left Array)
+ *         and pass the array (from mid to end) [end exclusive] to 2nd call of "mergesort" function (Right Array)
+ * Step 3: Now call the "merge" function to merge left and right array. 
+ * Step 4: In "merge" function put two pointers (i) for (left) and (j) for (right) at start of array.
+ * Step 5: and compare if (left[i] < right[j]) then left's element will be placed into merged column.
+ *         else right's elements will be placed.
+ * Step 6: while performing step 5 if any one from right or left array gets complited then remaining elements
+ *         from the other array must be put into merged array.
+ */        
 import java.util.Arrays;
 
 public class Merg_sort_recursion 
@@ -6,8 +27,9 @@ public class Merg_sort_recursion
     {
         int[] arr = {3,4,56,9,7};
         mergsortinplace(arr, 0, arr.length);
-        // int[] ans = mergsort(arr);
+        int[] ans = mergsort(arr);
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(ans));
     }
 
     static int[] mergsort(int[] input)
@@ -23,42 +45,41 @@ public class Merg_sort_recursion
         int[] right = mergsort(Arrays.copyOfRange(input, mid, input.length));
 
         return merge(left , right);
-
     }
 
-    private static int[] merge(int[] first, int[] second) 
+    private static int[] merge(int[] left, int[] right) 
     {
-        int[] mix = new int[first.length + second.length];
+        int[] mix = new int[left.length + right.length];
 
         int i = 0;
         int j = 0;
         int k = 0;
         
-        while(i < first.length && j < second.length)
+        while(i < left.length && j < right.length)
         {
-            if(first[i] < second[j])
+            if(left[i] < right[j])
             {
-                mix[k] = first[i];
+                mix[k] = left[i];
                 i++;
             }
             else
             {
-                mix[k] = second[j];
+                mix[k] = right[j];
                 j++;
             }
             k++;
         }
 
-        while(i<first.length)
+        while(i<left.length)
         {
-            mix[k] = first[i];
+            mix[k] = left[i];
             i++;
             k++;
         }
 
-        while(j<second.length)
+        while(j<right.length)
         {
-            mix[k] = second[j];
+            mix[k] = right[j];
             j++;
             k++;
         }
