8d3233d4fc7ca42456309ec5623c6f063077889e
Hardik7843 <hardikprajapati7843@gmail.com>
2023-07-07 12:30:05 +0530

    Cyclic sort explained

--- Sorting/Cyclic_sort.java
+++ Sorting/Cyclic_sort.java
@@ -8,7 +8,29 @@
  * Time complexity of cyclic sort: O(n)
  * number of comparisons: first the (n-1) swaps and checks for sorting array post
  * that it makes n checks for checking the elements.
+ */
+
+/*
+ * Idea: if array contains elements in range of 0 to n-1 or 1 to n.
+ * where n is length of the array. then we will know each element's 
+ * position in the array. 
  * 
+ * Steps takes place in cyclic sort
+ * 
+ * Intution : if elements are in range [1-n] inclusive. then position 
+ * (i) element is i-1. i.e it's correct position(corrpos).
+ * Step 1: Start for loop from start to end element of array.
+ * Step 2: Pick one element from the current pointer of for loop and calculate 
+ *         correct position i.e [ CorrPos = arr[i]-1 ] incase the array contains 
+ *         elements from (1 to n-1).
+ * Step 3: if elements at correct position(corrpos) is already there then increment the 
+ *         loop pointer (i). else swap the element at correct postion (corrpos) with 
+ *         element at loop pointer (i)
+ * by repeating this steps will give us the sorted array.
+ * Note : this algorithim can only handle array with distinct element.
+ * 
+ * 
+ * Step 3s
  */
 
 
@@ -21,9 +43,7 @@ public class Cyclic_sort
         int arr[] = {8,7,3,5,3,6,1,4};
         System.out.println("Hii");
         cyclic_sort(arr);
-
         System.out.println(Arrays.toString(arr));
-        
     }
 
     //Function for sorting given intger array
